94f1ed4fa2d46e5e308876b85976b5c1951f3b83
ridibrito <devoldbsb@gmail.com>
2024-03-16 08:30:56 -0300

    dominio

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
           name="description"
           content="Domine os 3 pilares fundamentais dos anúncios pagos com o Ads Master. Este curso abrangente oferece insights valiosos sobre plataformas de anúncios, estratégias criativas e planos de contingência para enfrentar desafios como bloqueios no Facebook Ads. Aprenda com especialistas em marketing digital e torne-se um mestre em tráfego pago. Inscreva-se agora e leve suas campanhas de anúncios para o próximo nível!"
         />
+        <meta
+          name="facebook-domain-verification"
+          content="tt4v71ncwcnmc49ev6yzlckdub5979"
+        />
         <link rel="icon" href="/icon.png" />
       </Head>
       <body>
