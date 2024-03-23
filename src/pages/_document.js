14253c423426d8467aa82295a7fc9a5bc0cd3fb2
ridibrito <devoldbsb@gmail.com>
2024-03-23 08:00:56 -0300

    hotjar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,6 +20,19 @@ export default class MyDocument extends Document {
           />
           <script
             dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3917676,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
               __html: `
                 !function(f,b,e,v,n,t,s)
                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
